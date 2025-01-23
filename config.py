b29a4b88e225bd5c5033d27ef361fa394aa03213
Robert Allaway <allaway@users.noreply.github.com>
2025-01-23 16:57:29 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+ZONE_HOME = {
+    "tl_y":  39.133323, # Top-Left Latitude (deg)
+    "tl_x":  -94.610844,  # Top-Left Longitude (deg)
+    "br_y": 39.045919, # Bottom-Right Latitude (deg)
+    "br_x": -94.515538 # Bottom-Right Longitude (deg)
+}
+LOCATION_HOME = [
+    39.075719, # Latitude (deg)
+    -94.572643, # Longitude (deg)
+    0.2911 # Altitude (km)
+]
+WEATHER_LOCATION = "Kansas City"
+OPENWEATHER_API_KEY = "814619bc2345cc67a5189455d203cb43"
+TEMPERATURE_UNITS = "metric"
+MIN_ALTITUDE = 100
+BRIGHTNESS = 30
+GPIO_SLOWDOWN = 2
+JOURNEY_CODE_SELECTED = ""
+JOURNEY_BLANK_FILLER = " ? "
