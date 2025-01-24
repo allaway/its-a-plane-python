0c51987f8c0db187db8c575fcd93f2f69e4d5bc6
Robert Allaway <allaway@users.noreply.github.com>
2025-01-24 14:38:54 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-ZONE_HOME = {
-    "tl_y":  39.21548886000494, # Top-Left Latitude (deg)
-    "tl_x":  -94.7485680531041,  # Top-Left Longitude (deg)
-    "br_y": 38.91538033413495, # Bottom-Right Latitude (deg)
-    "br_x": -94.30475416916127 # Bottom-Right Longitude (deg)
-}
+ZONE_HOME = {"tl_y":  39.21548886000494, "tl_x":  -94.7485680531041, "br_y": 38.91538033413495, "br_x": -94.30475416916127}
 LOCATION_HOME = [
     39.07573161534222, # Latitude (deg)
     -94.57260594572094, # Longitude (deg)
@@ -12,7 +7,7 @@ LOCATION_HOME = [
 WEATHER_LOCATION = "Kansas City"
 OPENWEATHER_API_KEY = "814619bc2345cc67a5189455d203cb43"
 TEMPERATURE_UNITS = "metric"
-MIN_ALTITUDE = 0
+MIN_ALTITUDE = 100
 BRIGHTNESS = 30
 GPIO_SLOWDOWN = 2
 JOURNEY_CODE_SELECTED = "MCI"
