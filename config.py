63ae3ecd5adfc4a51be67f37af6a9e31fc3f4f9f
Robert Allaway <allaway@users.noreply.github.com>
2025-01-24 12:33:26 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 ZONE_HOME = {
-    "tl_y":  39.133323, # Top-Left Latitude (deg)
-    "tl_x":  -94.610844,  # Top-Left Longitude (deg)
-    "br_y": 39.045919, # Bottom-Right Latitude (deg)
-    "br_x": -94.515538 # Bottom-Right Longitude (deg)
+    "tl_y":  39.21548886000494, # Top-Left Latitude (deg)
+    "tl_x":  -94.7485680531041,  # Top-Left Longitude (deg)
+    "br_y": 38.91538033413495, # Bottom-Right Latitude (deg)
+    "br_x": -94.30475416916127 # Bottom-Right Longitude (deg)
 }
 LOCATION_HOME = [
     39.075719, # Latitude (deg)
     -94.572643, # Longitude (deg)
-    0.2911 # Altitude (km)
+    0.1 # Altitude (km)
 ]
 WEATHER_LOCATION = "Kansas City"
 OPENWEATHER_API_KEY = "814619bc2345cc67a5189455d203cb43"
